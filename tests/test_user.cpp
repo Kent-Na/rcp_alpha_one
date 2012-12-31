f69d72c83186f70c4d12a2eb7c42df7840bcbc52
czel <czel@mac.com>
2012-12-31 18:44:22 +0900

    Fix test_user.cpp.

--- tests/test_user.cpp
+++ tests/test_user.cpp
@@ -1,7 +1,8 @@
 #include "../rcp_pch.h"
 #include "../rcp_utility.h"
 #include "../rcp_json.h"
-#include "../types/rcp_old_array.h"
+#include "../types/rcp_array.h"
+#include "../types/rcp_array_list.h"
 #include "../types/rcp_string.h"
 #include "../types/rcp_type_list.h"
 #include "../rcp_record.h"
@@ -10,11 +11,12 @@
 
 int test_user(){
 	{
-		rcp_record_ref rec = rcp_old_array_new_rec(rcp_ref_type);
-		rcp_old_array_ref ary = (rcp_old_array_ref)rcp_record_data(rec);
+		rcp_record_ref rec = rcp_record_new(rcp_ref_array);
+		rcp_array_ref ary = (rcp_array_ref)rcp_record_data(rec);
 		rcp_record_ref tmp;
 		tmp = rcp_string_new_rec("login");
-		rcp_old_array_append_data(
+		rcp_array_append_data(
+				rcp_ref_array,
 				ary,
 				(rcp_data_ref)&tmp);
 		rcp_permission_t pms = rcp_permission_from_array(rec);
