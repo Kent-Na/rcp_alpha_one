165510cf1609687c434f4d8a1b4a194eb278fc7e
Kent-Na <kent@tuna-cat.com>
2014-06-11 08:45:29 +0000

    Replace rcp_core with submodule.

--- rcp_core/def/rcp_dict.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef RCP_DEF_DICT
-#define RCP_DEF_DICT
-typedef struct rcp_dict_core *rcp_dict_ref;
-#endif
--- rcp_core/def/rcp_string.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef RCP_DEF_STRING
-#define RCP_DEF_STRING
-typedef struct rcp_string_core *rcp_string_ref;
-#endif
